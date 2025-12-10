58b9b34148b0df7dec0f29da651c725d3265f198
Raphaelly-Viana <vianaraphaelly@gmail.com>
2025-12-10 20:53:25 +1300

    exercise2

--- Exercise2/Routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const mathRoutes = require("./CalculatorRoutes");
-
-app.use("/", mathRoutes);
-
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
